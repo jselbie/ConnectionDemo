204b8b52fcfa4c6cb95e7c2c3caf28e42e459d50
jselbielocal <jselbielocal@jselbies-Mac-Pro.local>
2015-07-02 15:54:04 -0700

    Initial Commit

---
+++ ConnectionDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ConnectionDemo
+//
+//  Created by jselbielocal on 7/2/15.
+//  Copyright (c) 2015 jselbielocal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ConnectionDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ConnectionDemo
+//
+//  Created by jselbielocal on 7/2/15.
+//  Copyright (c) 2015 jselbielocal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
